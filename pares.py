677839e95299713312b31e94d21827d12439d569
Hadassatcs <hadassateles10@hotmail.com>
2024-09-27 19:20:58 -0300

    pares.py

--- pares.py
+++ pares.py
@@ -2,4 +2,58 @@ for numero in range(1, 101):
 
     if numero % 2 == 0:
         print(numero)
+
+PS C:\Users\Usuario\Desktop\PROJETO 1> & C:/Users/Usuario/AppData/Local/Programs/Python/Python312/python.exe "c:/Users/Usuario/Desktop/PROJETO 1/pares.py"
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+38
+40
+42
+44
+46
+48
+50
+52
+54
+56
+58
+60
+62
+64
+66
+68
+70
+72
+74
+76
+78
+80
+82
+84
+86
+88
+90
+92
+94
+96
+98
+100
+PS C:\Users\Usuario\Desktop\PROJETO 1> 
+
       
